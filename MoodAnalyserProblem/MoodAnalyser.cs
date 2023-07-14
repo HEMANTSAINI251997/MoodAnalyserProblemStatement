d6d3a7d663250e81b171466afc004f36ca7807ee
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-14 08:23:38 +0530

    [HEMANT]Add: Check mesge is SAD or Happy

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string massage)
+        {
+            //my name is sad 
+            if (massage.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}

b1db62758c00013ea787f36638c340c0b1b8c0dc
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-19 14:06:02 +0530

    [HEMANT]Add: Mood analyser using null reference exception

--- MoodAnalyserProblem/MoodAnalyseCustom_Exception.cs
+++ MoodAnalyserProblem/MoodAnalyseCustom_Exception.cs
@@ -1,22 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace MoodAnalyserProblem
-//{
-//    public class MoodAnalyseCustom_Exception:Exception
-//    {
-//        public enum MoodAnalyser_ExceptionType
-//        {
-//            NULL_MOOD,
-//            EMPTY_MOOD,
-//        }
-//        public MoodAnalyser_ExceptionType type; 
-//        public MoodAnalyseCustom_Exception(string message, MoodAnalyser_ExceptionType type) 
-//        {
-//            this.type = type;
-//        }
-//    }
-//}
+namespace MoodAnalyzer_Demo
+{
+    //Custom exception are user defined exceptions use to customize the exception as
+    //per the user needed.
+    public class MoodAnalyseCustom_Exception : Exception
+    {
+        public enum MoodAnalyser_ExceptionType
+        {
+            NULL_MOOD,
+            EMPTY_MOOD,
+        }
+        public MoodAnalyser_ExceptionType type;
+        //Base - This keyword  is used to access member of a base class  with in a derived class
+        public MoodAnalyseCustom_Exception(string message, MoodAnalyser_ExceptionType type) : base(message)
+        {
+
+            this.type = type;
+
+        }
+
+    }
+}
